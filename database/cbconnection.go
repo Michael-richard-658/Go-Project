85eef688f74b127effed07d4d2e71c5d6d235ccb
Michael <michaelrichard7020@gmail.com>
2025-07-26 11:21:18 +0530

    dbconnection

---
+++ database/cbconnection.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DB *sql.DB
+
+func DbConnection() {
+	var err error
+	dsn := "root:Richard658958!@tcp(127.0.0.1:3306)/GoProject"
+	DB, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("DB connection error:", err)
+	}
+
+	if err = DB.Ping(); err != nil {
+		log.Fatal("DB ping error:", err)
+	}
+
+	log.Println("MySQL connected.")
+}
--- main.go
+++ main.go
@@ -5,15 +5,17 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/Michael-richard-658/Go-project/database"
 	"github.com/Michael-richard-658/Go-project/handlers"
 )
 
 func main() {
+	database.DbConnection()
 	fmt.Println("Server is running on http://localhost:8000")
-
 	http.HandleFunc("/", handlers.HomeHandler)
 	http.HandleFunc("/createuser", handlers.CreateUser)
 	err := http.ListenAndServe(":8000", nil)
+	defer database.DB.Close()
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
